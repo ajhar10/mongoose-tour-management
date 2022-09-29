f52a42f1ac944e05e5a88454d925248020bf158d
ajhar10 <ajharrimon90@gmail.com>
2022-09-30 01:12:57 +0600

    tour middleware fixed

---
+++ middlewares/tour.viewcount.js
@@ -0,0 +1,14 @@
+const Tour = require("../models/Tour");
+
+const viewCounter = async (req, res, next) => {
+  const { id } = req.params;
+  const tour = await Tour.findById({ _id: id });
+  if (tour.viewesCount || tour.viewesCount == 0) {
+    await tour.set({ viewesCount: tour.viewesCount + 1 }).save();
+  } else {
+    await tour.set({ viewesCount: 1 }).save();
+  }
+  next();
+};
+
+module.exports = viewCounter;
--- routes/tour.route.js
+++ routes/tour.route.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
+const viewCounter = require("../middlewares/tour.viewcount");
 const tourControllers = require("../controllers/tour.controllers");
 
 router
   .route("/tours")
   .get(tourControllers.getTours)
   .post(tourControllers.createTour);
-router
-  .route("/tours/:id")
-  .get(tourControllers.getTourById)
-  .post(tourControllers.createTour);
-router.route("/tour/:id").get(tourControllers.updateATourPackage);
+router.route("/tours/:id").get(viewCounter, tourControllers.getTourById);
+router.route("/tour/:id").patch(tourControllers.updateATourPackage);
 router.route("/tour/cheapest").get(tourControllers.getCheapestTour);
 router.route("/tour/trending").get(tourControllers.getTrendingTour);
 
